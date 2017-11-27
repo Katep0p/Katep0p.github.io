c4ecddf946b43d50f68565818b502dd0c4ff8bb7
katarina <kwu@example.com>
2017-11-27 20:00:42 +0100

    added logic to stop interval after completion

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var index = 0;
 var start = 0;
 var finish = 0;
-
 var greeting = "Hi, I'm Kate,";
-
-setInterval(textAnimator, 200);
+var textAnimatorInterval = setInterval(textAnimator, 200);
 
 function textAnimator() {
   document.getElementById("header1").innerHTML = greeting.substring(start, finish);
   finish++;
+}
+
+if (finish === greeting.length){
+  clearInterval(textAnimatorInterval);
 }
