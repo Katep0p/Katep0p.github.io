cd712c09079cce08ad8b29aac29a16e35e14059d
katarina <kwu@example.com>
2018-03-24 12:01:55 +0100

    hhh

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var index = 0;
 var start = 0;
 var finish = 0;
-var greeting = 'Hi, I am ' + name.bold() + ',' + myProjects;
+var greeting = 'Hi, I am ' + name.bold() + ', I am glad to have you visit this rather minimalistic space, we are under reconstruction' + myProjects;
 var name = 'Kate ';
 var myOnlySolutionForThisComma = ",";
 var myProjects = '<br> check out my projects below.';
