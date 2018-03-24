11f1ffb9024c534ae63b7d886284a7f3711196ce
katarina <kwu@example.com>
2018-03-24 10:18:20 +0100

    realized  I made a mistake... go figure

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var start = 0;
 var finish = 0;
 var greeting = 'Hi, I am ';
 var name = '<span class="bold-text"> Kate</span>,';
-var res = name.slice(25,29);
 var myOnlySolutionForThisComma = ",";
 var myProjects = '<br> check out my projects below.';
 var textAnimatorInterval = setInterval(textAnimator, 150);
@@ -11,7 +10,7 @@ var textAnimatorInterval = setInterval(textAnimator, 150);
 function textAnimator() {
   document.getElementById("header1").innerHTML = greeting.substring(start, finish);
   finish++;
-  document.getElementById("header1").innerHTML = name.slice(27,30)
+  document.getElementById("header1").innerHTML = name.slice(25,29);
   //document.getElementById("header1").innerHTML = res;
 
   //if (finish === greeting.length){
