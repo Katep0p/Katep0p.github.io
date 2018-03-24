9789d0c4b5b9ce5e9c5d6f0dc76ba5166dc6f5ac
katarina <kwu@example.com>
2018-03-24 09:13:57 +0100

    made lots of changes I am so not sure of make any sense what so ever.. Im trying to find a way to get around the delay of k.....a......t.....e......

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var index = 0;
 var start = 0;
 var finish = 0;
-var greeting = 'Hi, I am <span class="bold-text">Kate</span>, <br> check out my projects below.';
-var textAnimatorInterval = setInterval(textAnimator, 200);
+var greeting = 'Hi, I am ';
+var name = <span class="bold-text">'Kate'</span>;
+var myProjects = <br> 'check out my projects below.';
+var textAnimatorInterval = setInterval(textAnimator, 150);
 
 function textAnimator() {
   document.getElementById("header1").innerHTML = greeting.substring(start, finish);
