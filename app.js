82646bde30414cffbb07b1c1bf340a6ce123da8c
katarina <kwu@example.com>
2018-03-24 10:12:33 +0100

    im getting pretty annoyed with the sloooooow update on web...server?

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var start = 0;
 var finish = 0;
 var greeting = 'Hi, I am ';
 var name = '<span class="bold-text"> Kate</span>,';
+var res = name.slice(27,30);
 var myOnlySolutionForThisComma = ",";
 var myProjects = '<br> check out my projects below.';
 var textAnimatorInterval = setInterval(textAnimator, 150);
@@ -10,13 +11,10 @@ var textAnimatorInterval = setInterval(textAnimator, 150);
 function textAnimator() {
   document.getElementById("header1").innerHTML = greeting.substring(start, finish);
   finish++;
-  if (finish === greeting.length){
-    clearInterval(textAnimatorInterval);
-  }
-}
+  document.getElementById("header1").innerHTML = name.slice(27,30)
+  //document.getElementById("header1").innerHTML = res;
 
-function myFunction() {
-    var str = "Hello world!";
-    var res = str.slice(0,2);
-    document.getElementById("header1").innerHTML = res;
+  //if (finish === greeting.length){
+    //clearInterval(textAnimatorInterval);
+//  }
 }
