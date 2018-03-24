fff0aa27323edb918e492242be7e7f91eb87230a
katarina <kwu@example.com>
2018-03-24 09:23:30 +0100

    please make this work.

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var index = 0;
 var start = 0;
 var finish = 0;
-var greeting = 'Hi, I am ';
-var name = <span class="bold-text">'Kate'</span>;
-var myProjects = <br> 'check out my projects below.';
+var greeting = 'Hi, I am <span class="bold-text">Kate</span>, <br> check out my projects below.';
 var textAnimatorInterval = setInterval(textAnimator, 150);
 
 function textAnimator() {
   document.getElementById("header1").innerHTML = greeting.substring(start, finish);
   finish++;
-  if (finish === greeting.length + name.length + myProjects.length){
+  if (finish === greeting.length){
     clearInterval(textAnimatorInterval);
   }
 }
