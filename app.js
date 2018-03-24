d2a5d427d3759c4b444334cc13373416660485bc
katarina <kwu@example.com>
2018-03-24 10:15:15 +0100

    having some trouble with uderstanding how javascript thinks...

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var start = 0;
 var finish = 0;
 var greeting = 'Hi, I am ';
 var name = '<span class="bold-text"> Kate</span>,';
-var res = name.slice(27,30);
+var res = name.slice(25,29);
 var myOnlySolutionForThisComma = ",";
 var myProjects = '<br> check out my projects below.';
 var textAnimatorInterval = setInterval(textAnimator, 150);
