b6768dd580516b662cdb30d626ee9fbbed0d17e5
katarina <kwu@example.com>
2018-03-24 11:48:43 +0100

    hhh

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var index = 0;
 var start = 0;
 var finish = 0;
-var greeting = 'Hi, I am ';
-var name = 'Kate, ';
+var greeting = 'Hi, I am ' + name + ',' + myProjects;
+var name = 'Kate ';
 var myOnlySolutionForThisComma = ",";
 var myProjects = '<br> check out my projects below.';
 var textAnimatorInterval = setInterval(textAnimator, 150);
