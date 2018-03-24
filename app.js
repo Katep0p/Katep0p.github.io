7a3acf5e9df83c3dece193ea8ce43a53ff79b34a
katarina <kwu@example.com>
2018-03-24 09:50:56 +0100

    i might be onto something now.. attempt 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var index = 0;
 var start = 0;
 var finish = 0;
 var greeting = 'Hi, I am ';
-var name = '<span class="bold-text">Kate</span>,';
+var name = '<span class="bold-text"> Kate</span>,';
 var myOnlySolutionForThisComma = ",";
 var myProjects = '<br> check out my projects below.';
 var textAnimatorInterval = setInterval(textAnimator, 150);
@@ -13,4 +13,10 @@ function textAnimator() {
   if (finish === greeting.length + name.length + myOnlySolutionForThisComma.length + myProjects.length){
     clearInterval(textAnimatorInterval);
   }
+}
+
+function myFunction() {
+    var str = "Hello world!";
+    var res = str.slice(0,2);
+    document.getElementById("header1").innerHTML = res;
 }
