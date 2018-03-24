28dfdedcb2e76461b8c4fb35317cc2248c6a39ab
katarina <kwu@example.com>
2018-03-24 09:36:38 +0100

    okey something is now terribly wrong...

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var index = 0;
 var start = 0;
 var finish = 0;
-var greeting = 'Hi, I am';
+var greeting = 'Hi, I am ';
 var name = '<span class="bold-text">Kate</span>,';
 var myOnlySolutionForThisComma = ",";
 var myProjects = '<br> check out my projects below.';
@@ -10,7 +10,7 @@ var textAnimatorInterval = setInterval(textAnimator, 150);
 function textAnimator() {
   document.getElementById("header1").innerHTML = greeting.substring(start, finish);
   finish++;
-  if (finish === greeting.length){
+  if (finish === greeting.length + name.length + myOnlySolutionForThisComma.length + myProjects.length){
     clearInterval(textAnimatorInterval);
   }
 }
