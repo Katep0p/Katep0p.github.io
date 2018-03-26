e26221f135dbd90b98dba20357fe7e24600ac4cc
katarina <kwu@example.com>
2018-03-26 19:47:17 +0200

    made some life changing edits to varios files

---
+++ safety copy for avoiding catastrophes.js
@@ -0,0 +1,16 @@
+var index = 0;
+var start = 0;
+var finish = 0;
+var greeting = 'Hi, I am ';
+var name = '<span class="bold-text"> Kate</span>,';
+var myOnlySolutionForThisComma = ",";
+var myProjects = '<br> check out my projects below.';
+var textAnimatorInterval = setInterval(textAnimator, 150);
+
+function textAnimator() {
+  document.getElementById("header1").innerHTML = greeting.substring(start, finish);
+  finish++;
+  if (finish === greeting.length + name.length + myOnlySolutionForThisComma.length + myProjects.length){
+    clearInterval(textAnimatorInterval);
+  }
+}
--- static/app.js
+++ static/app.js
@@ -1,17 +1,16 @@
 var index = 0;
 var start = 0;
 var finish = 0;
-var greeting = 'Hi, I am ' + name.bold() + '.' + smallerText;
+var greeting = 'Hi, I am';
 var name = 'Kate';
+var myProjects = 'check out my projects below.';
+var smallerText = 'I am glad to have you visit this rather minimalistic space of mine.';
 
-var smallerText = '<br> I am glad to have you visit this rather minimalistic space of mine.' + myProjects;
 
-
-var myProjects = '<br> check out my projects below.';
 var textAnimatorInterval = setInterval(textAnimator, 150);
 
 function textAnimator() {
-  document.getElementById("header1").innerHTML = greeting.substring(start, finish);
+  document.querySelector("#textBeforeNameElem").innerHTML = greeting.substring(start, finish+1);
   finish++;
   if (finish === greeting.length){
     clearInterval(textAnimatorInterval);
